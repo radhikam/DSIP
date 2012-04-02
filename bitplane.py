12b440ee8a0f05578ca9e0dce6fdcc9d2734355e
Radhika Mandra <radhika@bean.aero.iitb.ac.in>
2012-04-03 00:24:59 +0530

    Bitplane Running Succesfully

--- bitplane.py
+++ bitplane.py
@@ -1,29 +1,37 @@
 from pylab import *
-a = imread('Images/a.jpg')
+a = imread('Images/BnWFlower.jpg')
 p = double(a)
 r = int(raw_input('enter the bit you want to see 1=MSB and 8=LSB'))
-row,col,d = a.shape
-b = w = empty([row,col,d])
-z = p.size
-c = empty([z])
+row,col = a.shape
+w = empty([row,col])
 for i in range(1,row):
     for j in range(1,col):
-        for k in range(1,d):
-            x = int(p.item(i,j,k))
-            c = bin(x)
-            b[i,j,k] = c[r]
-            w = b
-            #w[i,j,k] = double(b.item(i,j,k))
-            if w.item(i,j,k)== 48: #or w(i,j,k) == 49:
-                w[i,j,k]=255
-            else:
-                w[i,j,k]=0
+        x = int(p.item(i,j))
+        m = binto8(x,r)
+        w[i,j] = double(m)
+        if w == 1:
+            w[i,j] = 255
+        else:
+            w[i,j] = 0
 figure(1)
 subplot(1,2,1)
 gray()
 imshow(a)
 title('Original Image')
 subplot(1,2,2)
-imshow(uint(w))
+gray()
+imshow(uint8(w))
 title('Bit Plane Slicing')
 show()
+
+
+def binto8(x,y):
+    # x=no. in the image array here p.item(x,y), y=r    
+    # code to make a binary number 8 bit binary and ftech the rth value from binary number
+    n = bin(int(x))
+    m = list(n)
+    count = len(m) - 2
+    for i in range(0,count):
+        m.insert(2,0)
+    m = m[y+1]        
+    return m
