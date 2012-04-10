9f86c42aecf347c612262671a0809c49c90d35ca
Radhika Mandra <mandra.radhika@gmail.com>
2012-04-10 23:55:44 +0530

    bitplane running successfuly

--- bitplane.py
+++ bitplane.py
@@ -1,4 +1,16 @@
 from pylab import *
+
+def binto8(x,y):
+    # x=no. in the image array here p.item(x,y), y=r    
+    # code to make a binary number 8 bit binary and ftech the rth value from binary number
+    n = bin(int(x))
+    m = list(n)
+    count = abs(len(m) - 2 - 8)
+    for i in range(0,count):
+        m.insert(2,0)
+    m = m[y+1]        
+    return m
+
 a = imread('Images/BnWFlower.jpg')
 p = double(a)
 r = int(raw_input('enter the bit you want to see 1=MSB and 8=LSB'))
@@ -9,7 +21,7 @@ for i in range(1,row):
         x = int(p.item(i,j))
         m = binto8(x,r)
         w[i,j] = double(m)
-        if w == 1:
+        if w[i,j] == 1:
             w[i,j] = 255
         else:
             w[i,j] = 0
@@ -23,15 +35,3 @@ gray()
 imshow(uint8(w))
 title('Bit Plane Slicing')
 show()
-
-
-def binto8(x,y):
-    # x=no. in the image array here p.item(x,y), y=r    
-    # code to make a binary number 8 bit binary and ftech the rth value from binary number
-    n = bin(int(x))
-    m = list(n)
-    count = len(m) - 2
-    for i in range(0,count):
-        m.insert(2,0)
-    m = m[y+1]        
-    return m
